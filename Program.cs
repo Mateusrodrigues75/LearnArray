ceedb16dee7072017da0d408c71a13c163413f04
Mateus Rodrigues <m.rodriguessilva75@outlook.com>
2020-11-19 11:11:47 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LearnArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Array");
+            string[] nomes = new string[3];
+            int[] idades = new int [3];
+            
+            for (var i = 0; i < 3; i++){
+                Console.WriteLine($"Digite o {i + 1}° Nome: ");
+                nomes [i] = Console.ReadLine();
+                Console.WriteLine("Digite a idade: ");
+                idades[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Nomes Cadastrados: ");
+
+            for (var i = 0; i < 3; i++){
+                Console.WriteLine($"Nome: {nomes[i]}");
+                Console.WriteLine($"Idade: {idades[i]}");
+            }
+        }
+    }
+}
